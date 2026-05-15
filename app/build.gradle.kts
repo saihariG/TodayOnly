f9f85a83030d7a9263421a79cca7b24aa5991797
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-15 16:28:35 -0400

    set minSDK to 28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.todayonly"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
