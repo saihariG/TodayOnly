a4efa6b6d72e205a3f39d7c92480b12756c12c76
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-18 19:50:44 -0400

    Hilt Entry Point

--- app/src/main/java/com/example/todayonly/presentation/MainActivity.kt
+++ app/src/main/java/com/example/todayonly/presentation/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.todayonly.presentation.Navigation.AppNavGraph
+import com.example.todayonly.presentation.navigation.AppNavGraph
 import com.example.todayonly.ui.theme.TodayOnlyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
