8816298ab0a03139f53b6c33e9dee5ae5a7972ef
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-19 01:39:12 -0400

    Empty Expired tasks screen

---
+++ app/src/main/java/com/example/todayonly/presentation/screens/composables/EmptyExpiredTasks.kt
@@ -0,0 +1,32 @@
+package com.example.todayonly.presentation.screens.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EmptyExpiredTasks(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            "No past tasks yet"
+        )
+        Spacer(Modifier.size(4.dp))
+        Text(
+            "Yesterday's tasks will appear here."
+        )
+    }
+
+}
