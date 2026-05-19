c200ae6f7510eea5d134460d4df4359ebc3458c3
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-18 21:32:53 -0400

    viewModel logic for add and observe

--- app/src/main/java/com/example/todayonly/presentation/viewmodel/TodayOnlyViewModel.kt
+++ app/src/main/java/com/example/todayonly/presentation/viewmodel/TodayOnlyViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todayonly.domain.Clock
 import com.example.todayonly.domain.repository.TaskRepository
+import com.example.todayonly.presentation.UiEvent
 import com.example.todayonly.presentation.uistates.TaskUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,17 +24,44 @@ class TodayOnlyViewModel @Inject constructor(
     private val _state = MutableStateFlow(TaskUiState())
     val state : StateFlow<TaskUiState> = _state.asStateFlow()
 
+    private val _uiEvent = MutableSharedFlow<UiEvent>()
+    val uiEvent = _uiEvent.asSharedFlow() // for one-shot events like snackbars
+
     init {
         loadTasks()
     }
 
     fun loadTasks() {
-
+        viewModelScope.launch {
+            try {
+                taskRepository.observeTasks().collect { tasks ->
+                    _state.value = TaskUiState(
+                        tasks = tasks
+                    )
+                }
+            } catch (e: Exception) {
+                _state.value = TaskUiState(
+                    error = "${e.message} Failed to load tasks"
+                )
+            }
+        }
     }
 
     fun addTask(title: String) {
         viewModelScope.launch {
-            val id = taskRepository.addTask(title)
+            val trimmedTitle = title.trim()
+
+            if(trimmedTitle.isBlank()) {
+                _uiEvent.emit(UiEvent.ShowSnackBar("Title cannot be empty"))
+                return@launch
+            }
+
+            try {
+                taskRepository.addTask(title)
+                _uiEvent.emit(UiEvent.ShowSnackBar("Task created"))
+            } catch (e: Exception) {
+                _uiEvent.emit(UiEvent.ShowSnackBar(e.message ?: "Failed to create task"))
+            }
         }
     }
 
