d9c4cdae42cc1410e94116f8930d778d37eda218
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-18 22:49:20 -0400

    Expired Tasks Entry

--- app/src/main/java/com/example/todayonly/presentation/screens/TodayTasksScreen.kt
+++ app/src/main/java/com/example/todayonly/presentation/screens/TodayTasksScreen.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.List
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -37,7 +39,8 @@ import com.example.todayonly.presentation.viewmodel.TodayOnlyViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodayTasksScreen(
-    viewModel: TodayOnlyViewModel = hiltViewModel()
+    viewModel: TodayOnlyViewModel = hiltViewModel(),
+    onOpenExpiredTasks: () -> Unit
 ) {
 
     val state by viewModel.state.collectAsState()
@@ -73,7 +76,11 @@ fun TodayTasksScreen(
                         )
                     }
                 },
-                
+                actions = {
+                    IconButton(onClick = onOpenExpiredTasks) {
+                        Icon(Icons.Outlined.List, contentDescription = "Expired Tasks")
+                    }
+                }
             )
         },
         floatingActionButton = {
