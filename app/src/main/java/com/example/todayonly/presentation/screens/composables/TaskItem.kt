af22d18b115d9e553c10c179bf64c8ee8af0221d
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-19 01:41:56 -0400

    Haptic Feedback for mark complete

--- app/src/main/java/com/example/todayonly/presentation/screens/composables/TaskItem.kt
+++ app/src/main/java/com/example/todayonly/presentation/screens/composables/TaskItem.kt
@@ -1,5 +1,6 @@
 package com.example.todayonly.presentation.screens.composables
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,15 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.unit.dp
 import com.example.todayonly.domain.model.Task
 
@@ -26,13 +29,22 @@ fun TaskItem(
     task: Task,
     onMarkedComplete : () -> Unit
 ) {
+
+    val haptic = LocalHapticFeedback.current
+
     Row(
         modifier = Modifier.fillMaxWidth().padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Box(
             modifier = Modifier.size(20.dp)
+                .border(
+                    width = if(task.isCompleted) 0.5.dp else 1.5.dp,
+                    color = MaterialTheme.colorScheme.outline,
+                    shape = CircleShape
+                )
                 .clickable {
+                    haptic.performHapticFeedback(HapticFeedbackType.LongPress)
                     onMarkedComplete()
                 },
             contentAlignment = Alignment.Center
