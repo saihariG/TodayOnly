4d826b5f59e18ff8ca5a4cfa9209510b8bd2f4a2
Sai Hari Krishnan <krishnansaihari@gmail.com>
2026-05-19 01:40:14 -0400

    Linked ExpiredTasks Screen in NavGraph

--- app/src/main/java/com/example/todayonly/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/todayonly/presentation/navigation/AppNavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.todayonly.presentation.screens.ExpiredTasksScreen
 import com.example.todayonly.presentation.screens.TodayTasksScreen
 
 @Composable
@@ -16,7 +17,14 @@ fun AppNavGraph(
         startDestination = "tasks"
     ) {
         composable("tasks") {
-            TodayTasksScreen()
+            TodayTasksScreen(
+                onOpenExpiredTasks = { navController.navigate("expired") }
+            )
+        }
+        composable("expired") {
+            ExpiredTasksScreen(
+                onBack = { navController.popBackStack() }
+            )
         }
     }
 }
